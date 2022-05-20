24f8e10f9ac2b6b1661eab5409a2e77118f48ffa
Truong <doanxuantruong747@gmail.com>
2022-05-20 22:37:00 +0700

    first commit

---
+++ src/components/ChoiceCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ChoiceCard({ gameItem, handleGameItemChange }) {
+  const { url, name } = gameItem;
+  return (
+    <img
+      onClick={() => { handleGameItemChange(gameItem) }}
+      className="choice"
+      src={url}
+      alt={name}
+    />
+  );
+}
+
+export default ChoiceCard;
